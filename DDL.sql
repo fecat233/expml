81f9a9f85f1b13f3ca1508cc757664ed1fa9be4c
fecat233 <goleer.zhangli@outlook.com>
2022-12-01 21:26:53 +0800

    init

---
+++ DDL.sql
@@ -0,0 +1,23 @@
+# 创建数据集
+CREATE DATABASE IF NOT EXISTS expml;
+
+# 删除数据库
+DROP DATABASE IF EXISTS expml;
+
+# 使用数据库
+USE expml;
+
+# 查询当前数据库
+SELECT DATABASE();
+
+# =======================================
+
+# DDL表操作 查询
+# 查询当前数据库所有的表
+SHOW TABLES;
+
+# 查询表结构
+DESC 表名;
+
+# 查询指定表的建表语句
+SHOW CREATE TABLE 表名;
